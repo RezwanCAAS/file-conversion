ee28d928ce1d4aa2c7e92ba349bec2765e114515
Rezwan Tariq <42382456+RezwanCAAS@users.noreply.github.com>
2024-02-20 18:11:48 +0300

    Add files via upload

---
+++ file_conversion.R
@@ -0,0 +1,27 @@
+
+library(readr)
+library(openxlsx)
+
+setwd("/Users/tariqr/Desktop/allele_matching/all_comparison/")
+getwd()
+
+txt_data <- read_delim("/Users/tariqr/Desktop/allele_matching/all_comparison/combined2.txt", delim = "\t")
+head(txt_data)
+
+
+write.xlsx(txt_data, "combined2.xlsx")
+
+
+file_list <- list.files(pattern = "output_.*_compared\\.txt")
+
+# Iterate through each file and convert to .xlsx
+for (file in file_list) {
+  # Read data from the current file
+  txt_data <- read_delim(file, delim = "\t")
+  
+  # Extract the filename without extension
+  filename <- tools::file_path_sans_ext(file)
+  
+  # Write data to .xlsx file
+  write.xlsx(txt_data, paste0(filename, ".xlsx"))
+}
